da0489c6686cfcfd8fbb7fa2135d8c6f3dcd4fdd
paul-hernandez99 <paul.hernandez@opendeusto.es>
2020-05-08 11:01:06 +0200

    Arreglo estructura

---
+++ src/Supervivencia.cpp
@@ -0,0 +1,82 @@
+#include "Supervivencia.h"
+
+void Supervivencia::pintarAsteroides(WINDOW* ventana, Asteroide* asteroides, int* num_ast)
+{
+
+}
+void Supervivencia::subirNivel(Asteroide* asteroides, int* num_ast)
+{
+
+}
+void Supervivencia::mostrarGameOver()
+{
+
+}
+int Supervivencia::menuSalida()
+{
+
+}
+WINDOW* Supervivencia::mostrarInfo()
+{
+
+}
+WINDOW* Supervivencia::mostrarJuego()
+{
+
+}
+void Supervivencia::tamanyoTerminal()
+{
+
+}
+void Supervivencia::jugar(Usuario* usuarios, int player)
+{
+
+}
+Supervivencia::Supervivencia()
+{
+
+}
+void Supervivencia::mostrarNivel(int* num_ast)
+{
+
+}
+void Supervivencia::mostrarVidaExtra()
+{
+
+}
+void Supervivencia::nuevaVidaExtra(VidaExtra* vidasExtra, int* num_vidasExtra)
+{
+
+}
+void Supervivencia::pintarVidasExtra(WINDOW* ventana, VidaExtra* vidasExtra, int* num_vidasExtra)
+{
+
+}
+int Supervivencia::choqueVidasExtra(WINDOW* ventana, NaveSupervivencia* nave, VidaExtra* vidasExtra, int* num_vidasExtra)
+{
+
+}
+void Supervivencia::actualizarS(WINDOW* ventana, NaveSupervivencia* nave)
+{
+
+}
+void Supervivencia::liberarMemoriaS(NaveSupervivencia* nave, Asteroide* asteroides, int* num_ast, VidaExtra* vidasExtra, int* num_vidasExtra, WINDOW* ventana)
+{
+
+}
+void Supervivencia::inicializarParametrosS(NaveSupervivencia* nave, Asteroide* asteroides, int* num_ast, VidaExtra* vidasExtra, int* num_vidasExtra)
+{
+
+}
+void Supervivencia::movimientosJugadorS(int tecla, NaveSupervivencia* nave)
+{
+
+}
+void Supervivencia::reestablecerValoresS(Asteroide* asteroides, int* num_ast)
+{
+
+}
+void Supervivencia::guardarPuntuacion(Usuario* usuarios, int player, int* num_ast)
+{
+	
+}
